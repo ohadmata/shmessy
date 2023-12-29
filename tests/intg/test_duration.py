8b1617d45757d2fa8510e7d1351504c276456ca6
ohadmata <ohad@pecan.ai>
2023-12-29 13:54:52 +0200

    nan improvments intg test

--- tests/intg/test_duration.py
+++ tests/intg/test_duration.py
@@ -1,5 +1,3 @@
-import os
-
 import pandas as pd
 from parametrization import Parametrization
 
@@ -9,15 +7,14 @@ from shmessy import Shmessy
 @Parametrization.autodetect_parameters()
 @Parametrization.case(
     name="Test demo data 1",
-    file_path="data/data_1.csv",
+    file_path="tests/data/data_1.csv",
 )
 @Parametrization.case(
     name="Test demo data 2",
-    file_path="data/data_2.csv",
+    file_path="tests/data/data_2.csv",
 )
-def test_duration_for_sample_file_should_be_less_than_300_ms(file_path, request):
-    tests_root = os.path.join(request.config.rootdir, "..")
-    df = pd.read_csv(os.path.join(tests_root, file_path))
+def test_duration_for_sample_file_should_be_less_than_350_ms(file_path):
+    df = pd.read_csv(file_path)
     result = Shmessy().infer_schema(df)
-    assert result.infer_duration_ms < 300
+    assert result.infer_duration_ms < 350
 
