f54ec11006a7388d50ad2db648dedeb6664e60e1
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-01-23 18:13:14 +0200

    added temporary files folder fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 import pytest
@@ -7,4 +8,13 @@ import pytest
 def files_folder() -> Path:
     folder_path = Path(__file__).parent / 'data'
     assert folder_path.exists()
+    return folder_path
+
+
+@pytest.fixture
+def tmp_files_folder() -> Path:
+    folder_path = Path(__file__).parent / 'tmp_folder_for_tests'
+    if folder_path.exists():
+        shutil.rmtree(folder_path)
+    folder_path.mkdir()
     return folder_path
