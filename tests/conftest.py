57079d65e35d0fb7c84fc0f7c6c5fd9b34ec07c7
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-01-24 09:50:11 +0200

    added cleaner fixture to clean test temporary generated files.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,18 @@ from pathlib import Path
 import pytest
 
 
+@pytest.fixture()
+def test_cleaner():
+    funcs = []
+
+    def add_func(func):
+        funcs.append(func)
+
+    yield add_func
+    for func in funcs:
+        func()
+
+
 @pytest.fixture
 def files_folder() -> Path:
     folder_path = Path(__file__).parent / 'data'
@@ -12,9 +24,10 @@ def files_folder() -> Path:
 
 
 @pytest.fixture
-def tmp_files_folder() -> Path:
+def tmp_files_folder(test_cleaner) -> Path:
     folder_path = Path(__file__).parent / 'tmp_folder_for_tests'
     if folder_path.exists():
         shutil.rmtree(folder_path)
     folder_path.mkdir()
+    test_cleaner(lambda: shutil.rmtree(folder_path))
     return folder_path
