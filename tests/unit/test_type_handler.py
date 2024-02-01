9e31129555602abd2d8ac80d2b61f6bf7b4d9a04
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-02-01 16:03:43 +0200

    added type handler tests.

---
+++ tests/unit/test_type_handler.py
@@ -0,0 +1,50 @@
+import hypothesis as hp
+import pandas as pd
+from hypothesis import strategies as st
+from hypothesis.extra.pandas import range_indexes, series
+
+import src.shmessy.types.base as shmessy_types
+from shmessy import TypesHandler
+from shmessy.types.base import BaseType
+from tests.unit.test_property_based import dtypes_st
+
+
+@st.composite
+def series_st(draw) -> pd.Series:
+    s = series(
+        dtype=draw(dtypes_st()),
+        index=range_indexes(min_size=2, max_size=5, ),
+    )
+    return draw(s)
+
+
+@st.composite
+def shmessy_type_st(draw, ) -> shmessy_types.BaseType:
+    types_handler = TypesHandler()
+    types_as_dict = types_handler._TypesHandler__types_as_dict
+    types = list(types_as_dict.values())
+    sht = draw(st.sampled_from(types))
+    return sht
+
+
+def test_handler_type_dict():
+    types_handler = TypesHandler()
+    types_as_dict = types_handler._TypesHandler__types_as_dict
+    values = types_as_dict.values()
+    values_base_types = {v.__class__.__bases__[0] for v in values}
+    assert {BaseType} == values_base_types
+
+
+@hp.given(pd_series=series_st(), field_name=st.text(min_size=1, max_size=10))
+def test_type_handler(pd_series, field_name):
+    type_handler = TypesHandler()
+    copied_series = pd_series.copy()
+    field = type_handler.infer_field(field_name=field_name, data=pd_series.values)
+    fixed_series = type_handler.fix_field(column=pd_series, inferred_field=field).copy()
+    assert copied_series.equals(pd_series)
+    new_fixed_series = type_handler.fix_field(column=fixed_series, inferred_field=field)
+    assert new_fixed_series.equals(fixed_series)
+    field_from_object = type_handler.infer_field(field_name=field_name, data=pd_series.astype(object).values)
+    fixed_from_object = type_handler.fix_field(column=pd_series.astype(object), inferred_field=field_from_object)
+    assert fixed_from_object.equals(fixed_series)
+    # todo shmessy dealing with object type
