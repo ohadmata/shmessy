db37c427c7fd2b856edb96317a93f408d26015e5
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2024-02-19 14:50:00 +0200

    move import statement to file level.
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,7 +1,7 @@
 import pytest
+from src.shmessy import TypesHandler  # if imported outside the function, coverage drops from 92% to 64%
 
 
 @pytest.fixture
 def type_handler():
-    from src.shmessy import TypesHandler  # if imported outside the function, coverage drops from 92% to 64%
     return TypesHandler(ignore_virtual_types=True)
