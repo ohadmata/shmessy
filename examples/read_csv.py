df09b6c0c62fbcec949c248eb96b9a59645f4c1b
ohadmata <ohad@pecan.ai>
2024-04-08 20:17:52 +0300

    types to ignore instead of virtual types ignore

--- examples/read_csv.py
+++ examples/read_csv.py
@@ -4,7 +4,7 @@ from utils import pretty_print_df, init_logger
 
 if __name__ == "__main__":
     init_logger()
-    shmessy = Shmessy(types_to_ignore=["unixTimestamp"])
-    df = shmessy.read_csv('../tests/data/test_timestamp.csv')
+    shmessy = Shmessy()
+    df = shmessy.read_csv('../tests/data/data_4.csv')
     inferred_schema = shmessy.get_inferred_schema()
     pretty_print_df(df=df, inferred_schema=inferred_schema)
