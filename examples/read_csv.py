0bc770d3e98440a3639b1b24fe60180ad9d5a7de
ohadmata <ohad@pecan.ai>
2024-05-29 13:54:58 +0300

    remove fixed schema parameter

--- examples/read_csv.py
+++ examples/read_csv.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     init_logger()
     shmessy = Shmessy()
     start_time = time.time()
-    df = shmessy.read_csv('../tests/data/data_30.csv')
+    df = shmessy.read_csv('../tests/data/data_1.csv')
     inferred_schema = shmessy.get_inferred_schema()
     pretty_print_df(df=df, inferred_schema=inferred_schema)
     print(f"Duration: {int((time.time() - start_time) * 1000)}")
