b4eeb207bdbc76bb28214ffacf79d6c1715a135a
ohadmata <ohad@pecan.ai>
2024-05-29 14:13:14 +0300

    change readme screenshots

--- examples/read_csv.py
+++ examples/read_csv.py
@@ -12,6 +12,4 @@ if __name__ == "__main__":
     df = shmessy.read_csv('../tests/data/data_1.csv')
     inferred_schema = shmessy.get_inferred_schema()
     pretty_print_df(df=df, inferred_schema=inferred_schema)
-    df1 = pandas.read_csv('../tests/data/data_1.csv')
-    pretty_print_df(df=df1)
     print(f"Duration: {int((time.time() - start_time) * 1000)}")
