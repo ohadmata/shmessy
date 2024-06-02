89c84ce767176970a48c6e6065f57a5f3770617e
ohadmata <ohad@pecan.ai>
2024-06-02 11:52:10 +0300

    thread executor

--- src/shmessy/__init__.py
+++ src/shmessy/__init__.py
@@ -1,13 +1,15 @@
+import concurrent.futures
 import locale
 import logging
 import time
+from concurrent.futures import Future
 from typing import BinaryIO, List, Optional, TextIO, Union
 
 import pandas as pd
 from pandas import DataFrame
 
 from .exceptions import exception_router
-from .schema import ShmessySchema
+from .schema import Field, ShmessySchema
 from .types_handler import TypesHandler
 from .utils import (
     _check_number_of_columns,
@@ -34,6 +36,7 @@ class Shmessy:
         fallback_to_null: Optional[bool] = False,
         use_csv_sniffer: Optional[bool] = True,
         fix_column_names: Optional[bool] = False,
+        max_number_of_workers: Optional[int] = 16,
     ) -> None:
         self.__types_handler = TypesHandler(types_to_ignore=types_to_ignore)
         self.__sample_size = sample_size
@@ -45,6 +48,7 @@ class Shmessy:
         self.__fallback_to_null = fallback_to_null
         self.__use_csv_sniffer = use_csv_sniffer
         self.__fix_column_names = fix_column_names
+        self.__max_number_of_workers = max_number_of_workers
 
         self.__inferred_schema: Optional[ShmessySchema] = None
 
@@ -56,6 +60,7 @@ class Shmessy:
         return self.__inferred_schema
 
     def infer_schema(self, df: DataFrame) -> ShmessySchema:
+        futures: list[Future] = []
         _check_number_of_columns(df=df, max_columns_num=self.__max_columns_num)
         start_time = time.time()
         df = _get_sampled_df(
@@ -63,10 +68,23 @@ class Shmessy:
             sample_size=self.__sample_size,
             random_sample=self.__use_random_sample,
         )
-        columns = [
-            self.__types_handler.infer_field(field_name=column, data=df[column].values)
-            for column in df
-        ]
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=self.__max_number_of_workers
+        ) as executor:
+            for column in df:
+                futures.append(
+                    executor.submit(
+                        self.__types_handler.infer_field,
+                        field_name=column,
+                        data=df[column].values,
+                    )
+                )
+            columns: list[Field] = []
+            for future in concurrent.futures.as_completed(futures):
+                if e := future.exception():
+                    raise e
+                columns.append(future.result())
+
         infer_duration_ms = int((time.time() - start_time) * 1000)
         inferred_schema = ShmessySchema(
             columns=columns, infer_duration_ms=infer_duration_ms
@@ -79,13 +97,27 @@ class Shmessy:
             _check_number_of_columns(df=df, max_columns_num=self.__max_columns_num)
             fixed_schema = self.infer_schema(df)
 
-            for column in fixed_schema.columns:
-                df[column.field_name] = self.__types_handler.fix_field(
-                    column=df[column.field_name],
-                    inferred_field=column,
-                    fallback_to_string=self.__fallback_to_string,
-                    fallback_to_null=self.__fallback_to_null,
-                )
+            futures: list[Future] = []
+            with concurrent.futures.ThreadPoolExecutor(
+                max_workers=self.__max_number_of_workers
+            ) as executor:
+
+                for column in fixed_schema.columns:
+                    futures.append(
+                        executor.submit(
+                            self.__types_handler.fix_field,
+                            column=df[column.field_name],
+                            inferred_field=column,
+                            fallback_to_string=self.__fallback_to_string,
+                            fallback_to_null=self.__fallback_to_null,
+                        )
+                    )
+
+                for future in concurrent.futures.as_completed(futures):
+                    if e := future.exception():
+                        raise e
+                    fixed_data, field_name = future.result()
+                    df[field_name] = fixed_data
 
             if self.__fix_column_names:
                 mapping = _fix_column_names(df)
@@ -117,7 +149,6 @@ class Shmessy:
                 dialect=dialect() if dialect else None,  # noqa
                 encoding=self.__reader_encoding,
             )
-
             return self.fix_schema(df=df)
 
         except Exception as e:
